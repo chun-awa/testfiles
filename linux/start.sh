af3de321fe648f9461568aec265ff3fcf8cd9c9a
chun-awa <1951474558@qq.com>
2022-10-06 18:42:04 +0800

    modified:   start.sh

--- linux/start.sh
+++ linux/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qemu-system-x86_64 -kernel vmlinuz-6.0 -initrd rootfs.bz2 -m 96 -append "vga=836 quiet"
+qemu-system-x86_64 -kernel vmlinuz-6.0 -initrd rootfs.bz2 -m 96 -append "vga=836 quiet" -smp 2
