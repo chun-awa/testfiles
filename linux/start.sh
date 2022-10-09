2e86de6537055b92d15eb96c1a923fbc74e463aa
chun-awa <1951474558@qq.com>
2022-10-09 19:03:59 +0800

    modified:   rootfs.bz2 	modified:   start.sh

--- linux/start.sh
+++ linux/start.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-qemu-system-x86_64 -kernel vmlinuz-6.0 -initrd rootfs.bz2 -m 96 -append "vga=836 quiet" -smp 2
+#!/bin/sh
+qemu-system-x86_64 $(if [ -e /dev/kvm ];then echo "-enable-kvm";fi) -kernel vmlinuz-6.0 -initrd rootfs.bz2 -m 96 -append "vga=836 quiet" -smp 2
