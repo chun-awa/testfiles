e32d4f635662a4eb5625a9185774956694c51186
chun-awa <1951474558@qq.com>
2023-01-01 17:22:14 +0800

    -

---
+++ ps1_3.sh
@@ -0,0 +1,30 @@
+RESET=$(printf "\033[0m")
+
+RED=$(printf "\033[91m")
+YELLOW=$(printf "\033[93m")
+GREEN=$(printf "\033[92m")
+CYAN=$(printf "\033[96m")
+BLUE=$(printf "\033[94m")
+MAGENTA=$(printf "\033[95m")
+WHITE=$(printf "\033[97m")
+
+prompt(){
+	exitcode=$?
+	printf $MAGENTA	
+	printf $(date '+%H:%M')
+	printf " "
+	printf $WHITE
+	printf $(tty)
+	printf " "
+	printf $CYAN
+	printf $(whoami)
+	printf "@"
+	printf $(hostname)
+	printf " "
+    printf $YELLOW
+	printf $(pwd)
+	printf $RESET
+	printf " > "	
+}
+
+PS1="\$(prompt)"
